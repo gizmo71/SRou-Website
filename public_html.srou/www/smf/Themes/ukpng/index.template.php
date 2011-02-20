6ff0332f4b83c2e0ae40a6972fcf886a212f48c7
Gizmo <Gizmo@bb5def06-8339-f94a-b570-60c0e87f3cdf>
2011-02-20 11:30:37 +0000

    Move all the copied stuff from shared.template.php back into the default template. Also filter .svn directories out of the project view.
    git-svn-id: https://svn.davegymer.org/repos/SRou/SimRacing.org.uk/trunk@2557 bb5def06-8339-f94a-b570-60c0e87f3cdf

--- public_html.srou/www/smf/Themes/ukpng/index.template.php
+++ public_html.srou/www/smf/Themes/ukpng/index.template.php
@@ -4,7 +4,11 @@ function srou_template_init() {
 	global $settings, $boardurl;
 
 	$settings['srou_layout_header'] = array_merge($settings['srou_layout_header'], array(
-		'imgSub'=>'GTR2 travels backwards in time',
+		'imgW'=>'580',
+		'imgH'=>'89',
+		'imgUrl'=>"$boardurl/Themes/ukpng/ukpng.jpg",
+		'imgSub'=>"by SimRacing.org.uk", // Was 'GTR2 travels backwards in time',
+		'imgSubStyle'=>'color: #e0e0ff',
 		'homeUrl'=>"$boardurl/index.php?action=LM2R&group=213&theme=34", //FIXME: use lm2 link fn?
 	));
 
