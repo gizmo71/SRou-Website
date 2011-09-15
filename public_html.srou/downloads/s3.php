57db9d4897e3dbdc04174c4239b52ea0162eccef
gizmo <gizmo@bb5def06-8339-f94a-b570-60c0e87f3cdf>
2011-09-15 17:53:18 +0000

    Add Amazon S3 link proxy for members only installers.
    git-svn-id: https://svn.davegymer.org/repos/SRou/SimRacing.org.uk/trunk@2630 bb5def06-8339-f94a-b570-60c0e87f3cdf

---
+++ public_html.srou/downloads/s3.php
@@ -0,0 +1,78 @@
+<?php
+require("../www/smf/SSI.php");
+
+// e.g. http://downloads.simracing.org.uk/s3.php/gtr2/fred.zip
+// e.g. http://downloads.simracing.org.uk/s3.php/gtl/susan.zip?bucket=wibble.davegymer.com
+// Old, direct form: http://awsdownloads.simracing.org.uk/gtr2/tracks/SRou_GTR2_FerretVille.exe
+// New, time-based form: http://downloads.simracing.org.uk/s3.php/gtr2/tracks/SRou_GTR2_FerretVille.exe
+
+// Need to make sure that new links have metadata: Content-Disposition: attachment; filename=SRou_GTR2_FerretVille.exe
+
+/*
+
+Note on the bucket policy:
+
+The old policy allowed downloads from SRou pages, which might include anonymously accessed ones.
+{
+	"Version": "2008-10-17",
+	"Id": "http referer policy",
+	"Statement": [
+		{
+			"Sid": "Allow get requests referred by SRou sites",
+			"Effect": "Allow",
+			"Principal": "*",
+			"Action": "s3:GetObject",
+			"Resource": "arn:aws:s3:::awsdownloads.simracing.org.uk/*",
+			"Condition": {
+				"StringLike": {
+					"aws:Referer": "*.simracing.org.uk/*"
+				}
+			}
+		}
+	]
+}
+
+The new policy uses active link generation to allow 24H access, and only to signed in members.
+{
+	"Version": "2011-09-15",
+	"Id": "http referer policy",
+	"Statement": [
+		{
+			"Sid": "Allow only via signed URLs",
+			"Effect": "Allow",
+			"Principal": {
+				"CanonicalUser": "e1e0b397c04953533c3e36e31d7fecb7f805cba6d1577b0d4e021463f311b32a"
+			},
+			"Action": "s3:GetObject",
+			"Resource": "arn:aws:s3:::awsdownloads.simracing.org.uk/*"
+		}
+	]
+}
+
+*/
+
+if ($ID_MEMBER <= 0) {
+	$me = "http://downloads.simracing.org.uk/s3.php{$_SERVER['PATH_INFO']}?{$_SERVER['QUERY_STRING']}";
+	echo "<P><B>You must be logged in to download this file.</B></P>";
+	ssi_login($me);
+	exit;
+}
+
+//$bucket = 'johnsmith';
+($bucket = $_REQUEST['bucket']) || ($bucket = 'awsdownloads.simracing.org.uk');
+//$key = '/photos/puppy.jpg';
+$key = $_SERVER['PATH_INFO'];
+$accessKey = 'AKIAIDZC5AXVTT6EVF2A';
+//$accessKey = '0PN5J17HBGZHT7JJ3X82'; // Amazon example
+$secretKey = 'RBXzg3VXxItqMd3h9fYRoqbdkvhd0cUgIXiNWEqf';
+//$secretKey = 'uV3F3YluFJax1cknvbcGwgjvx4QpvB+leU8dUj2o'; // Amazon example
+$expires = time() + 86400;
+//$expires = 1175139620;
+$canonical = "GET\n\n\n$expires\n/$bucket$key";
+$url = sprintf('http://%s%s?AWSAccessKeyId=%s&Signature=%s&Expires=%d',
+        $bucket, $key, $accessKey,
+        rawurlencode(base64_encode(hash_hmac('sha1', $canonical, $secretKey, true))),
+        $expires);
+//printf('<pre>%s</pre>', $url);
+header("Location: $url");
+?>
