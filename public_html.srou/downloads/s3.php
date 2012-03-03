0cb4b131ae3eb066b5b820780e12c95138573a58
gizmo <gizmo@bb5def06-8339-f94a-b570-60c0e87f3cdf>
2012-03-03 15:12:08 +0000

    Add ability to upload to S3 directly from a web page.
    git-svn-id: https://svn.davegymer.org/repos/SRou/SimRacing.org.uk/trunk@2668 bb5def06-8339-f94a-b570-60c0e87f3cdf

--- public_html.srou/downloads/s3.php
+++ public_html.srou/downloads/s3.php
@@ -58,20 +58,66 @@ if ($ID_MEMBER <= 0) {
 	exit;
 }
 
-$dryRun = ($_REQUEST['run'] == 'dry');
-($bucket = $_REQUEST['bucket']) || ($bucket = 'awsdownloads.simracing.org.uk');
-$key = $_SERVER['PATH_INFO'];
-$key = urlencode($key);
-$key = str_replace("%2F", "/", $key);
+$expires = time() + 86400;
+$defaultBucket = 'awsdownloads.simracing.org.uk';
+($bucket = $_REQUEST['bucket']) || ($bucket = $defaultBucket);
+$bucket = rawurlencode($bucket);
 $accessKey = 'AKIAIDZC5AXVTT6EVF2A';
 $secretKey = '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';
-$expires = time() + 86400;
-$canonical = "GET\n\n\n$expires\n/$bucket$key";
-$url = sprintf('http://%s%s?AWSAccessKeyId=%s&Signature=%s&Expires=%d',
-        $bucket, $key, $accessKey,
-        rawurlencode(base64_encode(hash_hmac('sha1', $canonical, $secretKey, true))),
-        $expires);
-header("Content-Type: text/plain");
-printf('%s', $url);
-$dryRun || header("Location: $url");
+
+switch ($_REQUEST['run']) {
+case 'upload1':
+	$expires = strftime("%FT%T.000Z", $expires);
+	$ID_MEMBER == 1 || die("You are not the Gizmo!");
+	header("Content-Type: text/html; charset=UTF-8");
+	$redirect = "http://downloads.simracing.org.uk/s3.php?run=uploaded";
+	$policy = <<<EOF
+{
+	"expiration": "$expires",
+	"conditions": [
+		{ "bucket": "$bucket" },
+		{ "x-amz-storage-class": "REDUCED_REDUNDANCY" },
+		[ "starts-with", "\$Content-Disposition", "attachment; filename=" ],
+		[ "starts-with", "\$key", "" ],
+		[ "starts-with", "\$success_action_redirect", "$redirect" ]
+	]
+}
+EOF
+	;
+	$policy = base64_encode($policy);
+	$signature = base64_encode(hash_hmac('sha1', $policy, $secretKey, true));
+?>
+	<form action="http://<?php echo $bucket; ?>" method="post" enctype="multipart/form-data">
+	Key <input type="text" size="100" name="key" value="gtr2/tracks/${filename}" /><br/>
+	<input type="hidden" name="AWSAccessKeyId" value="<?php echo $accessKey; ?>" />
+	<input type="hidden" name="Content-Disposition" value="attachment; filename=&quot;${filename}&quot;">
+	<input type="hidden" name="success_action_redirect" value="<?php echo htmlentities($redirect, ENT_QUOTES); ?>" />
+	<input type="hidden" name="signature" value="<?php echo $signature; ?>" />
+	<input type="hidden" name="x-amz-storage-class" value="REDUCED_REDUNDANCY" />
+	<input type="hidden" name="policy" value="<?php echo htmlentities($policy, ENT_QUOTES); ?>" />
+	File: <input type="file" name="file" /> <br />
+	<input type="submit" />
+</form>
+<?php
+	break;
+case 'uploaded':
+	header("Content-Type: text/plain");
+	printf('http://downloads.simracing.org.uk/s3.php/%s%s', $_REQUEST['key'], $bucket != $defaultBucket ? '?bucket=' . $bucket : '');
+	break;
+case 'dry':
+	$dryRun = true;
+default:
+	$dryRun = isset($dryRun) ? $dryRun : false;
+	$key = $_SERVER['PATH_INFO'];
+	$key = urlencode($key);
+	$key = str_replace("%2F", "/", $key);
+	$canonical = "GET\n\n\n$expires\n/$bucket$key";
+	$url = sprintf('http://%s%s?AWSAccessKeyId=%s&Signature=%s&Expires=%d',
+	        $bucket, $key, $accessKey,
+	        rawurlencode(base64_encode(hash_hmac('sha1', $canonical, $secretKey, true))),
+	        $expires);
+	header("Content-Type: text/plain");
+	printf('%s', $url);
+	$dryRun || header("Location: $url");
+}
 ?>
