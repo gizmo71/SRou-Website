9b417c679fb138aa1fae01845bfb7302e4e8e739
gizmo <gizmo@bb5def06-8339-f94a-b570-60c0e87f3cdf>
2013-07-24 16:43:30 +0000

    Page is rendered in Latin-1. Pff.
    git-svn-id: https://svn.davegymer.org/repos/SRou/SimRacing.org.uk/trunk@2747 bb5def06-8339-f94a-b570-60c0e87f3cdf

--- public_html.srou/www/lm2/index.php
+++ public_html.srou/www/lm2/index.php
@@ -1,6 +1,7 @@
 <?php require("../smf/SSI.php"); ?>
 <HTML>
 <HEAD>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
 <STYLE>
 .navbar { background: #dddddd; border: solid 1px }
 </STYLE>
