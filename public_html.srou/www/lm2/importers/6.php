3286b44821eaff6ef5e4fb5b84713e5474fc4952
gizmo <gizmo@bb5def06-8339-f94a-b570-60c0e87f3cdf>
2015-10-25 11:07:16 +0000

    Format RRE exports and work with new format.
    git-svn-id: https://svn.davegymer.org/repos/SRou/SimRacing.org.uk/trunk@2979 bb5def06-8339-f94a-b570-60c0e87f3cdf

--- public_html.srou/www/lm2/importers/6.php
+++ public_html.srou/www/lm2/importers/6.php
@@ -11,21 +11,26 @@ function showFileChoosers() {
 function doImport() {
 	global $location, $race_start_time;
 
-	$result = load_xml_file($_FILES['xml']);
-	$location = get_single_element_text($result, 'Track');
-	($race_start_time = strtotime(get_single_element_text($result, 'Time'))) || die("bad time");
+	$xpath = load_xml_file($_FILES['xml']);
+	$xpath->registerNamespace('m', 'http://schemas.datacontract.org/2004/07/CommunityMultiplayer.Services');
+	$result= $xpath->document->documentElement;
+	($result->tagName == 'MultiplayerRaceResult') || die("root node is not MultiplayerRaceResult");
+	($xpath->evaluate('string(m:Experience)') == 'RaceRoom Experience') || die("Does not appear to be RaceRoom Experience");
+	($location = $xpath->evaluate('string(m:Track)')) || die("No location");
+	($race_start_time = strtotime($xpath->evaluate('string(m:Time)'))) || die("No time");
 
 	$race = $qual = null;
-	foreach ($result->getElementsByTagName('MultiplayerRaceSession') as $session) {
-		($type = get_single_element_text($session, 'Type')) || die("No session type");
+//$result->getElementsByTagName('MultiplayerRaceSession')
+	foreach ($xpath->query('m:Sessions/m:MultiplayerRaceSession') as $session) {
+		($type = $xpath->evaluate('string(m:Type)', $session)) || die("No session type");
 		if ($type == 'Qualify') $qual = $session;
 		else if ($type == 'Race') $race = $session;
 	}
 echo "<P>Track is $location</P>\n";
 echo "<P>Race start time is $race_start_time</P>\n";
 
-	maybeParseXmlSession($race, true);
-	maybeParseXmlSession($qual, false);
+	maybeParseXmlSession($xpath, $race, true);
+	maybeParseXmlSession($xpath, $qual, false);
 
 	return true;
 }
@@ -40,54 +45,50 @@ function load_xml_file($file) {
 	// , DOMXML_LOAD_RECOVERING
 	($dom = DOMDocument::load($file['tmp_name'])) || die("Error while loading/parsing {$file['tmp_name']}");
 
-	$root = $dom->documentElement;
-
-	($root->tagName == 'MultiplayerRaceResult') || die("root node is not MultiplayerRaceResult");
-	(get_single_element_text($root, 'Experience') == 'RaceRoom Experience') || die("Does not appear to be RaceRoom Experience");
-
-	return $root;
+	return new DOMXPath($dom);
 } // End load_xml_file()
 
-function maybeParseXmlSession($session, $isRace) {
+function maybeParseXmlSession($xpath, $session, $isRace) {
 	if (!$session)
 		return;
 
-	foreach ($session->getElementsByTagName('MultiplayerRacePlayer') as $driver_entry) {
+	foreach ($xpath->query('m:Players/m:MultiplayerRacePlayer', $session) as $driver_entry) {
 		// Fake the old form...
 		$slot = array();
-		$slot['Driver'] = $slot['Lobby Username'] = get_single_element_text($driver_entry, 'Username');
+		$slot['Lobby Username'] = $xpath->evaluate('string(m:Username)', $driver_entry);
+		$slot['Driver'] = utf8_decode($xpath->evaluate('string(m:FullName)', $driver_entry)) ?: $slot['Lobby Username'];
 		if (!$slot['Driver']) $slot['Driver'] = '{unknown}';
-		$slot['Vehicle'] = get_single_element_text($driver_entry, 'Car');
-		//$slot['VehicleNumber'] = get_single_element_text($driver_entry, 'CarNumber');
-		//$slot['Team'] = get_single_element_text($driver_entry, 'TeamName');
-		//$slot['VehicleFile'] = get_single_element_text($driver_entry, 'VehFile');
-		//$slot['VehicleType'] = get_single_element_text($driver_entry, 'CarType');
-		//$slot['UpgradeCode'] = decodeUpgradeCode(get_single_element_text($driver_entry, 'UpgradeCode'));
-		//echo "<HR/>" . print_r($slot, true) . "\n";
+		$slot['Vehicle'] = $xpath->evaluate('string(m:Car)', $driver_entry);
+		//$slot['VehicleNumber'] = ?
+		//$slot['Team'] = ?
+		//$slot['VehicleFile'] = ?
+		//$slot['VehicleType'] = ?
+		//$slot['UpgradeCode'] = ?
+//echo "<HR/>" . print_r($slot, true) . "\n";
 
 		$entry = &lookup_entry($slot, $isRace);
 		if ($entry == null) continue;
 
-		$bestLapTime = get_single_element_text($driver_entry, 'BestLapTime', null);
-		if ($bestLapTime <= 0) $bestLapTime = null;
-		else $bestLapTime = bcdiv($bestLapTime, '1000', 3);
+		$bestLapTime = bcdiv($xpath->evaluate('string(m:BestLapTime[number(.) > 0])', $driver_entry) ?: null, '1000', 3);
 
 		if ($isRace) {
-			//$entry['raceTime'] = ;
-			//$entry['reason'] = "-1";
-			//$entry['raceLaps'] = get_single_element_text($driver_entry, 'Laps');
+			$laps = $xpath->query('m:RaceSessionLaps', $driver_entry)->item(0);
+			$entry['raceTime'] = bcdiv($xpath->evaluate('string(m:TotalTime[. != "-1"])', $driver_entry), '1000', 3) ?: null;
+			$entry['reason'] = $xpath->evaluate('string(m:FinishStatus)', $driver_entry) == 'Finished' ? null : 0;
+			$entry['raceLaps'] = $xpath->evaluate('count(m:MultiplayerRaceSessionLap[number(m:Time) > 0])', $laps);
 			$entry['raceBestLapTime'] = $bestLapTime;
-			//$entry['raceBestLapNo'] = ;
-			//$entry['Pitstops'] = get_single_element_text($driver_entry, 'Pitstops');
-			//$entry['GridPos'] = get_single_element_text($driver_entry, 'GridPos');
-			$entry['RacePos'] = get_single_element_text($driver_entry, 'Position');
+			$bestLapXPath = 'm:MultiplayerRaceSessionLap[m:Time = ../../m:BestLapTime]';
+			$entry['raceBestLapNo'] = $xpath->evaluate("count($bestLapXPath/preceding-sibling::m:MultiplayerRaceSessionLap) + number(boolean($bestLapXPath))", $laps) ?: null;
+			$entry['Pitstops'] = $xpath->evaluate('count(m:MultiplayerRaceSessionLap[m:PitStopOccured = "true"])', $laps);
+			//$entry['GridPos'] = ?
+			$entry['RacePos'] = $xpath->evaluate('string(m:Position)', $driver_entry);
 		} else {
 			//$entry['qualLaps'] = ;
 			$entry['qualBestLapTime'] = $bestLapTime;
 			//$entry['qualBestLapNo'] = ;
 		}
 
-		//echo "<BR/> = <B>" . print_r($entry, true) . "</B>\n";
+//if (!$isRace) echo "<BR/> = <B>" . print_r($entry, true) . "</B>\n";
 	}
 } // End maybeParseXmlSession()
 
