7f111387af1b6365d4edf43ecd62a13531cffdc1
gizmo <gizmo@bb5def06-8339-f94a-b570-60c0e87f3cdf>
2016-05-31 06:29:27 +0000

    Updates from live bin directory.
    git-svn-id: https://svn.davegymer.org/repos/SRou/SimRacing.org.uk/trunk@3023 bb5def06-8339-f94a-b570-60c0e87f3cdf

---
+++ bin/whatsnew.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+cd ~
+find bin public* \
+	\( -name error_log -o -name .ftpquota \
+	-o -wholename public_html.srou/www/smf/attachments \
+	-o -wholename public_ftp/backup \
+	-o -wholename public_html.davegymer.org/gallery/cpg15x/logs \
+	-o -wholename public_html.srou/www/mkportal/cache/\*.rss \
+	\) -prune -o \( -type f -mtime -8 -ls \)
