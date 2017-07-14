ce39aae71897fd1618d5ec972423e29b4e81a136
Gizmo <gizmo@davegymer.org>
2017-07-14 19:55:53 +0100

    Use database names and so on from common options.

--- dories/recreate-as-smf1.sh
+++ dories/recreate-as-smf1.sh
@@ -11,18 +11,18 @@ fi
 	# Percona: SET GLOBAL validate_password_policy = 'LOW';
 	# and on the end of create user: PASSWORD EXPIRE NEVER
 	# then afterwards: SET GLOBAL validate_password_policy = 'MEDIUM';
-	cat <<-"EOF"
+	cat <<-EOF
 		DROP USER IF EXISTS 'gizmo71_smf'@'%', 'gizmo71_backup'@'%';
-		CREATE USER 'gizmo71_smf'@'%'    IDENTIFIED BY 't$AQP1z[zUW8'
+		CREATE USER 'gizmo71_smf'@'%'    IDENTIFIED BY '${SROU_DB_PASSWD}'
 	                  , 'gizmo71_backup'@'%' IDENTIFIED BY 'ju5t1nca5e';
 	EOF
 
 	for db in smf lm2 ukgpl views; do
 		cat <<-EOF
-			DROP DATABASE IF EXISTS gizmo71_$db;
-			CREATE DATABASE gizmo71_$db DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-			GRANT ALL ON gizmo71_$db.* TO 'gizmo71_smf'@'%';
-			GRANT SELECT, LOCK TABLES, SHOW VIEW, CREATE TEMPORARY TABLES ON gizmo71_$db.* TO 'gizmo71_backup'@'%';
+			DROP DATABASE IF EXISTS ${SROU_DB_PREFIX}$db;
+			CREATE DATABASE ${SROU_DB_PREFIX}$db DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+			GRANT ALL ON ${SROU_DB_PREFIX}$db.* TO 'gizmo71_smf'@'%';
+			GRANT SELECT, LOCK TABLES, SHOW VIEW, CREATE TEMPORARY TABLES ON ${SROU_DB_PREFIX}$db.* TO 'gizmo71_backup'@'%';
 		EOF
 	done
 ) | mysql ${=SHARED_OPTIONS} ${=MIGRATE_LOGIN}
@@ -33,23 +33,26 @@ for type in 0 1 2; do for db in smf lm2 ukgpl views; do
 	sort =(ssh boxfish "ls -1 /var/backup/boxfish/boxfish_${db}_${type}_*.sql.gz") | while read sql; do
 		echo "** Processing $(basename $sql)..."
 		DB_HOST="--host $DB_HOSTS[$(($RANDOM % $#DB_HOSTS + 1))]" # Alternate(ish) to avoid filling binary log on one server.
-		ssh boxfish "zcat $sql" </dev/null | mysql ${=SHARED_OPTIONS} ${=SMF_LOGIN} ${=DB_HOST} gizmo71_${db}
+		ssh boxfish "zcat $sql" </dev/null | sed -e "s/gizmo71_\(smf\|lm2\)/${SROU_DB_PREFIX}\1/g" |
+		    mysql ${=SHARED_OPTIONS} ${=SMF_LOGIN} ${=DB_HOST} ${SROU_DB_PREFIX}${db}
 		echo "FLUSH LOGS;" | mysql ${=SHARED_OPTIONS} ${=MIGRATE_LOGIN} ${=DB_HOST}
 	done
 done; done
 
 (
-	mysqlshow ${=SHARED_OPTIONS/--batch/} ${=SMF_LOGIN} gizmo71_smf "mkp*" | grep mkp_ | cut -d' ' -f2 | grep -v mkp_pages | while read mkp; do
+	mysqlshow ${=SHARED_OPTIONS/--batch/} ${=SMF_LOGIN} ${SROU_DB_PREFIX}smf "mkp*" | grep mkp_ | cut -d' ' -f2 | grep -v mkp_pages | while read mkp; do
 		echo "DROP TABLE $mkp;"
 	done
 #TODO: remove this when we do it for real
 	echo "UPDATE smf_settings SET value = CONCAT('SMF1 on the Dories', CHAR(10), value) WHERE variable = 'news';"
+#TODO: how to do the QA versions?
+#TODO: what about pathnames?
 	for url2s in www.simracing.org.uk www.ukgpl.com; do
 		for table in settings themes; do
 			echo "UPDATE smf_$table SET value = REPLACE(value, 'http://$url2s', 'https://$url2s') WHERE value LIKE '%http://$url2s%';"
 		done
 	done
-) | mysql ${=SHARED_OPTIONS} ${=SMF_LOGIN} gizmo71_smf
+) | mysql ${=SHARED_OPTIONS} ${=SMF_LOGIN} ${SROU_DB_PREFIX}smf
 
 rm -rf www public_html.ukgpl
 rm -rf www public_html.srou && mkdir public_html.srou
