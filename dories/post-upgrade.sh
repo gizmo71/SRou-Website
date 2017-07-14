44bda61b762c735280cdcfcc04f2ae4d4bc119a8
Gizmo <gizmo@davegymer.org>
2017-07-14 17:29:01 +0100

    UKGPL logo will use whatever protocol the caller is on.

--- dories/post-upgrade.sh
+++ dories/post-upgrade.sh
@@ -19,7 +19,7 @@ replace into smf_themes (id_member, id_theme, variable, value) values
 	(0, 3, 'header_logo_url', '/images/ukgtr-gvw.jpg'),
 	(0, 4, 'header_logo_url', '/images/srou-pops.gif'),
 	(0, 5, 'header_logo_url', '/images/ukgtl-shark.gif'),
-	(0, 6, 'header_logo_url', 'http://www.ukgpl.com/images/ukgpl.jpg'),
+	(0, 6, 'header_logo_url', '//www.ukgpl.com/images/ukgpl.jpg'),
 	(0, 33, 'header_logo_url', '/smf/Themes/ukir/ukir.jpg'),
 	(0, 34, 'header_logo_url', '/smf/Themes/ukpng/ukpng.jpg');
 UPDATE smf_boards JOIN _map_board_themes ON id_board = board SET id_theme = theme;
