b3a442dbab4c23ca11b2c159cd312bba6e8f2d16
Gizmo <gizmo@davegymer.org>
2017-07-30 19:25:52 +0100

    Turn on subject display in SMF2.

--- dories/post-upgrade.sh
+++ dories/post-upgrade.sh
@@ -26,7 +26,9 @@ UPDATE smf_boards JOIN _map_board_themes ON id_board = board SET id_theme = them
 UPDATE smf_settings SET value = '1,3,4,5,6,33,34' WHERE variable IN ('enableThemes', 'knownThemes');
 UPDATE smf_settings SET value = '4' WHERE variable = 'theme_guests';
 UPDATE smf_settings SET value = '0' WHERE variable = 'minimize_files';
-INSERT INTO smf_settings (variable, value) VALUES ('force_ssl', '2');
+INSERT INTO smf_settings (variable, value) VALUES
+	('force_ssl', '2'),
+	('subject_toggle', '1');
 EOF
 
 sed -i -e s"/db_type = 'mysql'/db_type = 'mysqli'/" \
