5ebf62e26348d188425c078e4968d06f03246bff
Gizmo <gizmo@davegymer.org>
2017-08-04 08:06:28 +0100

    Dump old users and don't create a backup user. SQL backups are now done by the flatfish.

--- dories/recreate-db-users.zsh
+++ dories/recreate-db-users.zsh
@@ -8,7 +8,8 @@
 	# then afterwards: SET GLOBAL validate_password_policy = 'MEDIUM';
 	cat <<-EOF
 		DROP USER IF EXISTS 'gizmo71_smf'@'%', 'gizmo71_backup'@'%';
-		CREATE USER 'gizmo71_smf'@'%'    IDENTIFIED BY '${SROU_DB_PASSWD}'
-	                  , 'gizmo71_backup'@'%' IDENTIFIED BY 'ju5t1nca5e';
+		DROP USER IF EXISTS '${SROU_DB_PREFIX}smf'@'%', '${SROU_DB_PREFIX}backup'@'%';
+		CREATE USER '${SROU_DB_PREFIX}smf'@'%' IDENTIFIED BY '${SROU_DB_PASSWD}';
 	EOF
 ) | mysql ${=SHARED_OPTIONS} ${=MIGRATE_LOGIN}
+# No longer need '${SROU_DB_PREFIX}backup'@'%' IDENTIFIED BY 'ju5t1nca5e'
