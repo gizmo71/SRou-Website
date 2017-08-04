e50fac9a49bb3d6743ae5dcba3a30656ce3add1e
Gizmo <gizmo@davegymer.org>
2017-08-04 08:11:21 +0100

    Remove obsolete backup user altogether.

--- dories/recreate-db-users.zsh
+++ dories/recreate-db-users.zsh
@@ -7,9 +7,7 @@
 	# and on the end of create user: PASSWORD EXPIRE NEVER
 	# then afterwards: SET GLOBAL validate_password_policy = 'MEDIUM';
 	cat <<-EOF
-		DROP USER IF EXISTS 'gizmo71_smf'@'%', 'gizmo71_backup'@'%';
-		DROP USER IF EXISTS '${SROU_DB_PREFIX}smf'@'%', '${SROU_DB_PREFIX}backup'@'%';
+		DROP USER IF EXISTS '${SROU_DB_PREFIX}smf'@'%';
 		CREATE USER '${SROU_DB_PREFIX}smf'@'%' IDENTIFIED BY '${SROU_DB_PASSWD}';
 	EOF
 ) | mysql ${=SHARED_OPTIONS} ${=MIGRATE_LOGIN}
-# No longer need '${SROU_DB_PREFIX}backup'@'%' IDENTIFIED BY 'ju5t1nca5e'
