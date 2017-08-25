6b6c66bed32dd65fcb4fc97fca0721b29d8f6a61
Dave 'Gizmo' Gymer <github@davegymer.org>
2017-08-25 09:27:22 +0100

    Don't need to stripslashes because current PHP doesn't do magic quotes and SMF has been told to suffer the raw behaviour.

--- public_html.srou/www/lm2/genfile.php
+++ public_html.srou/www/lm2/genfile.php
@@ -2,5 +2,5 @@
 preg_match('@^http://members.iracing.com/@', $_SERVER['HTTP_REFERER']) || die("bad referer {$_SERVER['HTTP_REFERER']}");
 header('Content-Type: text/csv');
 header("Content-Disposition: attachment; filename=\"{$_REQUEST['filename']}\"");
-echo stripslashes($_REQUEST['csv']);
+echo $_REQUEST['csv'];
 ?>
