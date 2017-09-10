8e978e89d74cc69cac45a55cb6f69c8042d57f8e
Dave 'Gizmo' Gymer <github@davegymer.org>
2017-09-10 18:48:12 +0100

    Give users 2 and 3 valid external and invalid emails respectively.

--- dories/recreate-as-smf1.sh
+++ dories/recreate-as-smf1.sh
@@ -43,7 +43,8 @@ done; done
 	done
 #TODO: remove these when we do it for real
 	echo "UPDATE smf_settings SET value = CONCAT('SMF1 on the Dories in $SROU_ROOT', CHAR(10), value) WHERE variable = 'news';"
-	echo "UPDATE smf_members SET realName = REVERSE(realName), emailAddress = 'smf2test@simracing.org.uk', hideEmail = 0;"
+	echo "UPDATE smf_members SET realName = REVERSE(realName), hideEmail = 0, emailAddress =
+		CASE id_member WHEN 2 THEN 'micra.geo@yahoo.com' WHEN 3 THEN 'dgymer23@ford.com' ELSE 'smf2test@simracing.org.uk' END;"
 ) | mysql ${=SHARED_OPTIONS} ${=SMF_LOGIN} ${SROU_DB_PREFIX}smf
 
 rm -rf www public_html.ukgpl
