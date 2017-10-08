a2935f209e5cc59bd16155c2d332547d456e5e2e
Dave Gymer <gizmo71@boxfish.aquarium.davegymer.org>
2017-10-08 09:14:56 +0100

    Change mailcheck to show some text if the link doesn't work.

--- public_html.srou/www/mailcheck.php
+++ public_html.srou/www/mailcheck.php
@@ -1,17 +1,20 @@
 <?php
 require_once("smf/SSI.php");
 
+header("Content-Type: text/html");
+
+$ID_MEMBER || die("You must be logged in to call this script");
+
 $query = db_query("INSERT INTO {$lm2_ukgpl_prefix}mailcheck(id_member) VALUES ($ID_MEMBER)" , __FILE__, __LINE__);
 $id = db_insert_id();
 mysql_free_result($query);
 
-$subject = rawurlencode("Mail check from {$user_info['username']} (id $id)");
-$body = rawurlencode("My registered email is {$user_info['email']}\r\nMy member ID number is $ID_MEMBER");
-header("Location: mailto:mailcheck@mail.simracing.org.uk?subject=$subject&body=$body");
+$to = "mailcheck@mail.simracing.org.uk";
+$subject = "Mail check from {$user_info['username']} (id $id)";
+$body = "My registered email is {$user_info['email']}\r\nMy member ID number is $ID_MEMBER";
+$link = "mailto:$to?subject=" . rawurlencode($subject) . "&body=" . rawurlencode($body);
 
-/*
-echo "<br/><pre>";
-print_r($user_info);
-echo "</pre>";
-*/
+echo "<script>//window.location='", json_encode($link), ";</script>",
+     "If you email client has not automatically opened with an email, please send an email to <a href='mailto:",
+     $link, "'>", $to, "</a> with a subject of '", $subject, "'. Thanks!";
 ?>
