4baf85d835035d365843e3bb6cb85d5e748ac38d
Dave Gymer <gizmo71@boxfish.aquarium.davegymer.org>
2017-11-25 18:26:01 +0000

    Note about calling.

--- public_html.srou/www/batch/updateDrivers.php
+++ public_html.srou/www/batch/updateDrivers.php
@@ -1,3 +1,8 @@
+<!--
+Called using a cron job along the lines of:
+*/15 * * * * cd /tmp && wget --header="Host: www.simracing.org.uk" -O - http://127.0.0.1/batch/updateDrivers.php >rebuildDriverCache.log 2>&1
+-->
+
 <?php
 require('../smf/SSI.php');
 require("$boarddir/../lm2/include.php");
