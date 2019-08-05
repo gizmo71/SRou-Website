6d2572edb28ee88c16faa9103107545ecb973492
Dave 'Gizmo' Gymer <github@davegymer.org>
2019-08-05 17:46:49 +0000

    Rearrange driver update to avoid warnings.

--- public_html.srou/www/batch/updateDrivers.php
+++ public_html.srou/www/batch/updateDrivers.php
@@ -1,15 +1,13 @@
-<!--
-Called using a cron job along the lines of:
-*/15 * * * * cd /tmp && wget --header="Host: www.simracing.org.uk" -O - http://127.0.0.1/batch/updateDrivers.php >rebuildDriverCache.log 2>&1
--->
-
 <?php
+header('Content-Type: text/plain');
+
 require('../smf/SSI.php');
 require("$boarddir/../lm2/include.php");
 
-header('Content-Type: text/plain');
-
 rebuild_driver_cache();
+
+// Called using a cron job along the lines of:
+// */15 * * * * cd /tmp && wget --header="Host: www.simracing.org.uk" -O - http://127.0.0.1/batch/updateDrivers.php >rebuildDriverCache.log 2>&1
 ?>
 
 Done
