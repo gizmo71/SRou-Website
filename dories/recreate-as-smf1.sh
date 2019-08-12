66aa6b1d18fbd8c138b441d6d2855fbb1ea063b1
Dave 'Gizmo' Gymer <github@davegymer.org>
2019-08-12 19:43:04 +0000

    Through away SRou QA sync from Boxfish and recreate directly from live production files.

--- dories/recreate-as-smf1.sh
+++ dories/recreate-as-smf1.sh
@@ -7,8 +7,7 @@ fi
 
 . ./restore-database.sh
 
-echo 'TODO: not from Boxfish, but from SRouProd.'
-exit 1
+PROD_ROOT=/home/srouprod
 
 rm -rf www public_html.ukgpl
 rm -rf www public_html.srou && mkdir public_html.srou
@@ -16,21 +15,22 @@ rm -rf www public_html.srou && mkdir public_html.srou
 cd public_html.srou
 
 # --exclude="500*"
-(cd $HOME/boxfish/public_html.srou && tar -c -f - --exclude="smf/Packages/backups/*.tar.gz" --exclude='mkportal/cache/*.rss' www) | tar xvf -
-(cd $HOME/boxfish/public_html.srou && tar -c -f - --exclude="*/*.zip" replays) | tar xvf -
-(cd $HOME/boxfish/public_html.srou && tar -c -f - downloads) | tar xvf -
+TAR="tar -c -f - --exclude=error_log --exclude=*~"
+(cd $PROD_ROOT/public_html.srou && ${=TAR} --exclude="smf/Packages/backups/*.tar.gz" --exclude='mkportal/cache/*.rss' www) | tar xvf -
+(cd $PROD_ROOT/public_html.srou && ${=TAR} --exclude="*/*.zip" replays) | tar xvf -
+(cd $PROD_ROOT/public_html.srou && ${=TAR} downloads) | tar xvf -
 
 cd www/smf
 
-#sed <$HOME/boxfish/public_html.srou/www/smf/Settings.php >Settings.php \
+#sed <$PROD_ROOT/public_html.srou/www/smf/Settings.php >Settings.php \
 #    -e s"/maintenance = 0/maintenance = 1/" \
 # Get annoying warnings with PHP 5.5 and above. Should be fixed in SMF 2.1.
 for file in index SSI; do
-#	sed <$HOME/boxfish/public_html.srou/www/smf/${file}.php >${file}.php -e s"/E_ALL/E_ALL \& ~E_DEPRECATED \& ~E_NOTICE/"
+#	sed <$PROD_ROOT/public_html.srou/www/smf/${file}.php >${file}.php -e s"/E_ALL/E_ALL \& ~E_DEPRECATED \& ~E_NOTICE/"
 done
 
 cd $SROU_ROOT
-(cd $HOME/boxfish && tar -c -f - public_html.ukgpl) | tar xvf -
+(cd $PROD_ROOT && ${=TAR} public_html.ukgpl) | tar xvf -
 
 git status
 
