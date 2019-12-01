aa621195735325c244acd4f8b5badde8be0335a0
Dave 'Gizmo' Gymer <github@davegymer.org>
2019-12-01 18:39:44 +0000

    Add some comments on other hooks of interest.

--- {etc}/SMF-mods/srou-smf-2.1/main/install.php
+++ {etc}/SMF-mods/srou-smf-2.1/main/install.php
@@ -7,6 +7,7 @@ elseif (!defined('SMF'))
 
 // updateSettings(...)?
 
+// https://wiki.simplemachines.org/smf/Integration_hooks
 add_integration_function('integrate_pre_include', '$sourcedir/Subs-LM2.php', TRUE);
 add_integration_function('integrate_menu_buttons', 'lm2AddButtons', TRUE);
 add_integration_function('integrate_actions', 'lm2AddActions', TRUE);
@@ -15,5 +16,6 @@ add_integration_function('integrate_pre_profile_areas', 'lm2AddProfileAreas', TR
 add_integration_function('integrate_display_topic', 'lm2AddTopicDetails', TRUE);
 add_integration_function('integrate_load_theme', 'loadThemeData', TRUE);
 //add_integration_function('integrate_theme_include', '$boarddir/../layout-header.php', TRUE);
+// integrate_buffer - will this be easier than editing the index.template.php?
 
 ?>
