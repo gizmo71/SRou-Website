8b8f223ee2118d5fd61b38f0540daf93a8f5d9fa
Dave 'Gizmo' Gymer <github@davegymer.org>
2019-12-22 13:30:09 +0000

    Remove defunct SMF 2.1 preparation SQL.

--- dories/prepare-smf2.sh
+++ dories/prepare-smf2.sh
@@ -2,12 +2,7 @@
 
 . ./common.sh
 
-# Remove the first one when doing this for real!
 cat <<EOF | mysql ${=SHARED_OPTIONS} ${=SMF_LOGIN} ${SROU_DB_PREFIX}smf
-update smf_settings set value = REPLACE(value, 'SMF1 on the ', 'SMF2 on the ') where variable = 'news';
---update smf_settings set value = CONCAT('{"1":"', value, '"}') where variable = 'attachmentUploadDir';
---update smf_boards set name = CONCAT('2_', name);
---update smf_messages set subject = CONCAT('2_', subject);
 delete from smf_themes where id_member > 0;
 delete from smf_themes where variable = 'header_logo_url' and id_theme in (4, 5, 6, 33, 34, 3);
 delete from smf_log_banned;
