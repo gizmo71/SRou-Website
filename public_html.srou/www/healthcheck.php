5d60ba646aaf5019342e3d5986ca99dfa6a5d7de
Dave 'Gizmo' Gymer <github@davegymer.org>
2020-06-30 19:16:42 +0000

    Something odd happened to the healthcheck during upgrade...

--- public_html.srou/www/healthcheck.php
+++ public_html.srou/www/healthcheck.php
@@ -1,6 +1,12 @@
 <?php
+// wget -S -O - --header 'Host: check.haproxy.davegymer.org' http://web/healthcheck
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+http_response_code(500);
+//ob_end_flush();
+require_once("/srv/public_html.srou/www/smf/SSI.php");
 http_response_code(503);
-require_once("{$_ENV['SROU_ROOT']}/public_html.srou/www/smf/SSI.php");
 if (($db = mysqli_connect($db_server, $db_user, $db_passwd, $db_name))) {
 	mysqli_close($db);
 	http_response_code(200);
