26e08885c3a31627efed4e6c992532670fd3132a
Dave 'Gizmo' Gymer <github@davegymer.org>
2021-02-06 14:23:52 +0000

    Season 38 UKGPL updates.

---
+++ public_html.ukgpl/pages/aboutS37.php
@@ -0,0 +1,88 @@
+<?php $block_title = 'Welcome to UKGPL!'; $page_title = 'UKGPL'; ?>
+
+<DIV ALIGN="CENTER">
+
+<H1>The UK Grand Prix Legends Championships</H1>
+
+<P>This is the home page of UKGPL, a United Kingdom based online racing league for fans of Grand Prix Legends.</P>
+
+<H1><IMG WIDTH="800" HEIGHT="379" SRC="/images/Watkins1968.jpg"></H1>
+
+<H1>Registration for Season 37 is OPEN!<br></H1>
+<H2>This season we will be offering something for everyone. From the older 1955 GP machines through the 1965, 1966, 1967 and 1968 open wheel single seaters to the 1967 Sports Cars. We will run a variety of race lengths with shorter sprint races and longer Grand Prix distance events.</H2>
+
+<P align="justify">
+The league is still going strong despite the advent of dozens of new sims. Grand Prix Legends models some of the best cars from the fifties, sixties and early seventies and does it exceptionally well. Despite having been around since 1998, Grand Prix Legends still attracts a loyal following and UKGPL offers the opportunity to race open wheel cars from the original 1967 season and cars from the 1955, 1965, 1966, 1968 and 1969 seasons. The 67 Sports Cars, 66 Can-Ams and 71 Can-Ams provide an additional challenge.</p>
+
+<P align="justify">
+UKGPL is founded around our 1967 format championship which represents the pinnacle of achievement and this season we will be running two divisions for the 1967 F1 cars. One division will involve 50 minute Sprint races whilst the other will involve 90 minute Grand Prix distance races. UKGPL will also run championships for the 1955 GP, 1967 Sports Cars and a mixed championship featuring rounds for the 1965 F1, 1966 F1, 1967 F2 and 1968 F1 Cars. Our aim is simply to have some fun racing the 50s and 60s machines.</p>
+
+<P align="justify">
+We are now an international league having drivers from many countries. Our 67F1 championship will again be a joint venture with GPL Brazil. We used the GPL Brazil format and scoring system last season, so this time we will be using the UKGPL token system for the Sprint races and chassis allocation for the Grand Prix distance events.
+There is something to suit everyone, both newcomers to online racing and those more experienced, so join our forum introduce yourself and enjoy the racing.</P>
+
+<P align="justify">
+In order to allow drivers to plan their sim racing activities, we have adopted a regular calendar for UKGPL races. We will run two seasons per calendar year. The "Spring" season of ten rounds will start on the second Sunday of the year and the "Autumn" season will start in July so that a season of ten rounds can be completed before the end of the calendar year. The "Joe Shaw Memorial" races will be held in the week after the Easter holidays.</p>	
+
+<P align="justify">
+<H1>The provisional dates for future events are:</H1>
+<H2>Season 38: Start 10th January 2021 - Finish 8th June 2021</H2>
+<H2>Joe Shaw Memorial Races: 11th April 2021 and 13th April 2021</H2>
+<H2>Season 39: Start 4th July 2021 - Finish 14th December 2021</H2>
+<P align="center">Summer Break: Start 28th July 2021 - Finish 29th August 2021</p>
+</p>
+<P align="justify"><p>
+
+<P align="justify">
+<H1>To sign-up for Season 37, click on these links:</H1>	
+<p>
+<A HREF="//www.simracing.org.uk/smf/index.php?topic=20452.0">1955 Grand Prix</A><br>
+<A HREF="//www.simracing.org.uk/smf/index.php?topic=20472.0">Mixed Open Wheel</A><br>
+<A HREF="//www.simracing.org.uk/smf/index.php?topic=20502.0">1967 Formula 1 - Sprint</A><br>
+<A HREF="//www.simracing.org.uk/smf/index.php?topic=20492.0">1967 Formula 1 - Grand Prix</A><br>
+<A HREF="//www.simracing.org.uk/smf/index.php?topic=20462.0">1967 Sports Cars</A><br>
+</P>
+
+<P align="justify">
+If all of this has not whetted your appetite why not take a look at our 10th Anniversary video compiled by Vlad Vosblod.
+If you are ex-UKGPL and are contemplating a return, see if you can spot yourself!</P>
+
+<P>
+<object width="480" height="385"><param name="movie" value="https://www.youtube.com/v/PENRvoR-NU8&hl=en&fs=1&rel=0"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="https://www.youtube.com/v/PENRvoR-NU8&hl=en&fs=1&rel=0" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="480" height="385"></embed></object>
+</P>
+
+<HR WIDTH="75%" />
+
+<P>Our Chief Moderator is <A HREF="//www.simracing.org.uk/smf/index.php?action=profile;u=1643">Phil Thornton</A>.</P>
+
+<P>Please address applications to join to any of the moderating team, whose profiles are below</P>
+
+<P>Our web master is <A HREF="//www.simracing.org.uk/smf/index.php?action=profile;u=1">Dave 'Gizmo' Gymer</A>.</P>
+
+<HR WIDTH="75%" />
+
+<P align="justify">If you are a seasoned Grand Prix Legends campaigner and would simply like to know more about our
+league please browse our <A HREF='//www.simracing.org.uk/smf/index.php#6'>forum</A>.
+If you are a complete beginner you may find the <A HREF='/index.php/starter'>Getting Started</A> page
+and <A HREF='/index.php/training'>Training Article</A> helpful.  If you're ready to start racing
+on-line there are some invaluable hints and tips in our <A HREF='//www.simracing.org.uk/smf/index.php?topic=6787.0'>New Drivers thread</A>.</P>
+
+<P>Existing members may wish to jump directly to the list of <A HREF='//www.simracing.org.uk/smf/index.php?action=unread;all'>all unread posts</A>.</P>
+
+<HR WIDTH="75%" />
+
+<P><BIG>We do not have any funded race servers.  All our servers are provided by our members free of charge for the benefit of the drivers. These members are:
+
+<BR/>	
+<A HREF="//www.simracing.org.uk/smf/index.php?action=profile;u=5475"> Cookie</A>
+<BR/>
+<A HREF="//www.simracing.org.uk/smf/index.php?action=profile;u=6703"> Dave Curtis</A>
+<BR/>
+<A HREF="//www.simracing.org.uk/smf/index.php?action=profile;u=1643"> Phil Thornton</A>
+</BIG></P>
+
+
+<P><SMALL>Disclaimer: Grand Prix Legends is copyright Sierra Sports and Papyrus.
+<BR/>Any files on this site are the property of their author, no works may be duplicated or distributed without the express permission of the author.</SMALL></P>
+
+</DIV>
