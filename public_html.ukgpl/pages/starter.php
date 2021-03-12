25a879680c047d2744ee9ab03209de4ef08cc8db
Dave 'Gizmo' Gymer <github@davegymer.org>
2021-03-12 09:48:48 +0000

    UKGPL update. Call the whitespace police!

--- public_html.ukgpl/pages/starter.php
+++ public_html.ukgpl/pages/starter.php
@@ -21,3 +21,72 @@
 <H3>Tuned connection</H3>
 
 <P>Finally, you need a <B>good quality connection</B>. The central driving force behind the creation of UKGPL was the idea that by restricting the series to UK-based drivers and using a UK-based server, we could keep connection quality up and latency down to enjoy high quality racing, greatly reducing the warping and clock smashes which characterize GPL internet play.   However with the faster connection speeds afforded by modern broadband facilites we can now welcome drivers worldwide but the race times will be best suited to UK based drivers.</P>
+
+<H3>Trouble Shooting</H3>
+
+<P>If after following all the guidelines in the <A HREF="https://www.simracing.org.uk/smf/index.php?topic=6787.msg118356#msg118356">New Drivers</A> thread you are experiencing problems, there is help available from the other UKGPL Members via the <A HREF="https://www.simracing.org.uk/smf/index.php#6">forums</A>. Typical areas where drivers experience problems and specific help is available are:
+<UL>
+  <LI> Windows 10 and updates.
+  <LI> Linux.
+  <LI> <A HREF="https://www.simracing.org.uk/smf/index.php?topic=12808.0">Framerate using the 60fps patch</A>.
+  <LI> <A HREF="https://www.simracing.org.uk/smf/index.php?topic=24321.0">Bad framerate on-line</A>.
+  <LI> Crash to Desktop (CTD).
+  <LI> Screen Freeze.
+  <LI> Steering Wheels and USB connections.  
+  <LI> <A HREF="https://www.simracing.org.uk/smf/index.php?topic=23771.0">iGOR</A>.  
+</UL>
+</P>
+
+<P>
+There are 3 ways on-line races end prematurely. It is important to make a note of exactly what happened, because the remedy in each case will be different:
+<UL>
+   <LI>Crash to Desktop (CTD) - GPL stops mid race, shuts down without prompting and the user is returned to the Operating System (desktop) not iGOR.
+   <LI>Screen Freeze - GPL stops mid race, the graphics appear frozen and the sound turns into a buzz that increases in volume and pitch. The only way to get out of this situation is to use Task Manager to kill GPL or to do a hardware reset (i.e power off the PC).
+   <LI>Disco - The client is disconnected from the server. GPL stops mid race and returns to the GPL Multiplayer menu. The user can then close GPL using the normal red button and the user is returned to iGOR.
+</UL>
+</P>
+
+<P>
+To ensure the other members understand the issue fully, it would be helpful to provide as much of the following information as possible:
+<UL>
+  <LI>Operating System
+  <UL>
+    <LI>Windows 7/Windows 10/Linux
+    <LI>Last updates
+	<LI>Graphics Driver
+  </UL>
+  <LI>GPL Installation 
+  <UL>
+    <LI>How installed? - Disk + 1.08 installer - Demo or disk + manual installed
+    <LI>HDD, SSD or external HD?
+    <LI>Rasterizer used?
+  </UL>
+    <LI>Hardware
+  <UL>
+    <LI>CPU @clocks
+    <LI>RAM type and GB
+    <LI>GPU name + driver
+    <LI>HDD/SSD size + age!
+  </UL>
+  <LI> Problem
+   <UL>
+    <LI>A description of the problem. 
+   </UL>
+</UL>
+</P>
+
+<P>
+There are numerous utilities available that can be used to identify hardware issues. These include:
+<UL>
+  <LI>Windows
+  <UL>
+    <LI><A HREF="https://www.hwinfo.com/download/">HWiNFO64 (portable)</A>.
+    <LI><A HREF="https://openhardwaremonitor.org/">Open Hardware Monitor</A>.
+	<LI><A HREF="https://www.techpowerup.com/download/techpowerup-gpu-z/">GPU-Z</A>.
+  </UL>
+  <LI>Linux
+   <UL>
+    <LI><A HREF="https://mintguide.org/system/870-cpu-x-shows-information-on-cpu-motherboard-and-more.html">CPU-X</A>.
+    <LI><A HREF="https://wpitchoune.net/psensor/">Psensor</A>.
+  </UL> 
+</P>
