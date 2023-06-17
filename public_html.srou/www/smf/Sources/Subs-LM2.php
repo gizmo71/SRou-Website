f1c82cc475a28f0e09a5b24e05dc45673f993346
Dave 'Gizmo' Gymer <github@davegymer.org>
2023-06-17 17:28:11 +0000

    Remove the TeamSpeak viewer.

--- public_html.srou/www/smf/Sources/Subs-LM2.php
+++ public_html.srou/www/smf/Sources/Subs-LM2.php
@@ -180,14 +180,6 @@ function lm2FormatEventDetails() {
 			. "</SPAN>");
 
 		if ($row['entries_c'] == 0) {
-			if ($settings['theme_id'] != 6) {
-				echo '<div style="float: right" id="ts3viewer_14187" style=""> </div>
-				     <script src="https://static.tsviewer.com/short_expire/js/ts3viewer_loader.js"></script>
-				     <script>var ts3v_url_1 = "https://www.tsviewer.com/ts3viewer.php?ID=14187&text=757575&text_size=10&text_family=1&text_s_color=000000&text_s_weight=normal&text_s_style=normal&text_s_variant=normal&text_s_decoration=none&text_i_color=&text_i_weight=normal&text_i_style=normal&text_i_variant=normal&text_i_decoration=none&text_c_color=&text_c_weight=normal&text_c_style=normal&text_c_variant=normal&text_c_decoration=none&text_u_color=000000&text_u_weight=normal&text_u_style=normal&text_u_variant=normal&text_u_decoration=none&text_s_color_h=&text_s_weight_h=bold&text_s_style_h=normal&text_s_variant_h=normal&text_s_decoration_h=none&text_i_color_h=000000&text_i_weight_h=bold&text_i_style_h=normal&text_i_variant_h=normal&text_i_decoration_h=none&text_c_color_h=&text_c_weight_h=normal&text_c_style_h=normal&text_c_variant_h=normal&text_c_decoration_h=none&text_u_color_h=&text_u_weight_h=bold&text_u_style_h=normal&text_u_variant_h=normal&text_u_decoration_h=none&iconset=default";
-ts3v_display.init(ts3v_url_1, 14187, 100);
-</script>';
-			}
-			
 			if ($row['server_starter']) {
 				$request2 = db_query("
 					SELECT driver_member AS id_member
